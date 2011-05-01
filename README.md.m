9f902c3011d17e1a9f2b587a248f1fb51eb19872
kolia <kolia.sadeghi@gmail.com>
2011-05-01 02:46:54 -0400

    CAST algorithm now has a chance of working.  Currently stuck at initial temperature.

---
+++ README.md.m
@@ -0,0 +1,11 @@
+USEAGE
+------
+
+
+Place `peach_data.mat` and `cone_params.mat` in the folder where the code lives.
+
+cd to that folder in matlab and run `main_CAST.m`
+This just sets up 'cone_map' for the first time.
+
+Now run:
+`cone_map = CAST(cone_map) ;`
--- main_CAST.m
+++ main_CAST.m
@@ -1,9 +1,12 @@
-% load peach_data    % contains 'stas'
-% load cone_params   % contains 'cone_params'
-% 
-% cone_map = exact_LL_setup(stas,cone_params) ; % cone_map, aka PROB or data
+% PREPARE cone_map
+
+load peach_data    % contains 'stas'
+load cone_params   % contains 'cone_params'
+
+cone_map = exact_LL_setup(stas,cone_params) ; % cone_map, aka PROB or data
 
 cone_map.betas  = make_deltas( 0.5,1,1,50) ;
 cone_map.deltas = make_deltas(0.85,1,1,length(cone_map.betas)) ;
 
-cone_map = CAST(cone_map) ;
+% THEN RUN THIS:
+% cone_map = CAST(cone_map) ;
