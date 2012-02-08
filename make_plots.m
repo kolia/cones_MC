7060bae6fb67cf451c3acd6c27d9b5834794a5ff
kolia <kolia.sadeghi@gmail.com>
2012-02-08 18:25:27 -0500

    mkdir appropriately named folder for plots.

--- make_plots.m
+++ make_plots.m
@@ -1,8 +1,23 @@
-% reap  % load greed, mcmc and cast from hpc cluster
+function make_plots(greed,mcmc,cast,cone_map)
+
+if nargin<4
+    cone_map = remake_cone_map(greed.initX) ;
+end
+
+filename = ['plots_for_' cone_map_string(cone_map)] ;
+mkdir( filename )
+
+here = pwd ;
+
+cd(filename)
 
 plot_LL_ncones( greed , mcmc , cast )
 plot_Greedy_MCMC_CAST( greed , mcmc , cast )
 
-[sta,invww] = denoised_sta( cast{1}.initX , cast{1}.X.dX , cast{1} ) ;
+[sta,invww] = denoised_sta( greed.initX , cast{1}.X.dX , cone_map ) ;
 
 make_sta_plots( sta , invww, 'denoised' )
+
+cd(here)
+
+end
