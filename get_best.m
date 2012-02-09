54288f8720c0f51f12a87730588bc4fcf9dfb41a
Kolia Sadeghi <kolia@stat.columbia.edu>
2012-02-09 13:46:10 -0500

    get_best robust to empty Xs

--- get_best.m
+++ get_best.m
@@ -1,4 +1,4 @@
-function [x,y,states] = get_best( Xs )
+function [x,y,states,keep] = get_best( Xs )
 % Xs: cell struct with either bestX or X field.
 % Each bestX (or X) is either a struct, in which case
 % x and y are taken from the 'N_cones' and 'll' fields resp.
@@ -8,28 +8,39 @@ function [x,y,states] = get_best( Xs )
 x      = zeros(numel(Xs),1) ;
 y      = zeros(numel(Xs),1) ;
 states = cell(numel(Xs),1) ;
+
+keep = false(numel(Xs),1) ;
 for i=1:numel(Xs)
-    if isfield(Xs{i},'bestX')
-        X = Xs{i}.bestX ;
-    else
-        X = Xs{i}.X ;
-    end
-    if isa(X, 'cell')
-        j = 0 ;
-        bestll = -1e10 ;
-        for k=1:numel( X )
-            if X{k}.ll > bestll
-                j = k ;
-            end
+    if isa(Xs{i},'struct')
+        if isfield(Xs{i},'bestX')
+            X = Xs{i}.bestX ;
+        else
+            X = Xs{i}.X ;
         end
-        x(i) = X{j}.N_cones ;
-        y(i) = X{j}.ll ;
-        states{i} = X{j}.state ;
-    else
-        x(i) = X.N_cones ;
-        y(i) = X.ll ;
-        states{i} = X.state ;
+        if isa(X, 'cell')
+            j = 0 ;
+            bestll = -1e10 ;
+            for k=1:numel( X )
+                if X{k}.ll > bestll
+                    j = k ;
+                end
+            end
+            x(i) = X{j}.N_cones ;
+            y(i) = X{j}.ll ;
+            states{i} = X{j}.state ;
+        else
+            x(i) = X.N_cones ;
+            y(i) = X.ll ;
+            states{i} = X.state ;
+        end
+        if x(i)>0
+            keep(i) = true ;
+        end
     end
 end
+
+x = x(keep) ;
+y = y(keep) ;
+states = states(keep) ;
 
 end
