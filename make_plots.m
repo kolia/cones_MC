9944c726b074fa38e55d54dfec37f9f27c6e5c2f
Kolia Sadeghi <kolia@stat.columbia.edu>
2012-02-09 16:01:03 -0500

    make_plots adds cone_map argument to plotters

--- make_plots.m
+++ make_plots.m
@@ -11,8 +11,8 @@ here = pwd ;
 
 cd(filename)
 
-plot_LL_ncones( greed , mcmc , cast )
-plot_Greedy_MCMC_CAST( greed , mcmc , cast )
+plot_LL_ncones( greed , mcmc , cast , cone_map )
+plot_Greedy_MCMC_CAST( greed , mcmc , cast , cone_map.NICE )
 
 [sta,invww] = denoised_sta( greed.initX , cast{1}.X.dX , cone_map ) ;
 
