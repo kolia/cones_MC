9026fc304835122a2009b59709f6432593e63677
kolia <kolia.sadeghi@gmail.com>
2012-03-19 12:36:18 -0400

    plotting negative LL in white, -Inf in black

--- plotable_evidence.m
+++ plotable_evidence.m
@@ -1,12 +1,19 @@
 function evidence = plotable_evidence( evidence )
 
+infinite = ~isfinite(evidence) ;
+evidence(~isfinite(evidence)) = 1e-6 ;
+
 z = max(evidence,[],3) ;
-inds  = logical(z<0) ;
+inds  = logical(z<=0) ;
 inds  = inds(:) ;
 inds3 = [inds ; inds ; inds] ;
-evidence(inds3) = -[z(inds) ; z(inds) ; z(inds)] ;
+% evidence(inds3) = -[z(inds) ; z(inds) ; z(inds)] ;
+evidence(inds3) = 0 ;
 
 evidence = evidence-min(evidence(:)) ;
 evidence = evidence / max(evidence(:)) ;
+
+evidence(inds3) = 1 ;
+evidence(infinite) = 0 ;
 
 end
