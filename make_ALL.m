d5df726e8058f75a3c2b2c703eac4f7e4253380f
kolia <kolia.sadeghi@gmail.com>
2012-03-28 12:52:51 -0400

    make_ALL now makes more…

--- make_ALL.m
+++ make_ALL.m
@@ -12,24 +12,29 @@ load('peach/cast_peach_NROI2_ROI12_support3_SS4_5e05iters')
 
  
 this = pwd ; 
-try 
-    make_plots( greed_peach_NROI2_ROI12_support3_SS4_5e05iters,...
-                mcmc_peach_NROI2_ROI12_support3_SS4_5e05iters,...
-                cast_peach_NROI2_ROI12_support3_SS4_5e05iters ) ; 
-catch e, cd(this) ; end
+% try
+make_plots( greed_peach_NROI2_ROI12_support3_SS4_5e05iters,...
+            mcmc_peach_NROI2_ROI12_support3_SS4_5e05iters,...
+            cast_peach_NROI2_ROI12_support3_SS4_5e05iters ) ; 
+% catch e, cd(this) ; end
 
-addpath('../agricola')
-reap
+try
+    load('george/cast_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters')
+    load('george/mcmc_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters')
+catch
+    addpath('../agricola')
+    reap
+
+    save('george/cast_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters',...
+         'cast_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters')
+
+    save('george/mcmc_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters',...
+         'mcmc_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters')
+end
 
 [greed_george,cone_map_george,base_str_george] = main_CAST( 0 ) ;
-
-save('george/cast_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters',...
-     'cast_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters')
  
-save('george/mcmc_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters',...
-     'mcmc_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters')
- 
-this = pwd ; 
+this = pwd ;
 try 
     make_plots( greed_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters,...
                 mcmc_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters,...
