eb22268cd0154bca68e3eddd22cc9450b520a307
kolia <kolia.sadeghi@gmail.com>
2012-03-29 14:19:26 -0400

    plot_cones_field is now template based

--- plot_cone_field.m
+++ plot_cone_field.m
@@ -13,17 +13,21 @@ r = floor( 255*cc(x+M0*(y-1)) ./ alpha ) ;
 g = floor( 255*cc(  M0*M1 + x+M0*(y-1)) ./ alpha ) ;
 b = floor( 255*cc(2*M0*M1 + x+M0*(y-1)) ./ alpha ) ;
 
-evidence = print_evidence( PROB.NICE ) ;
-
 svg = sprints('<use xlink:href="#1" transform="translate(%f %f)" stroke="rgb(%d,%d,%d)" opacity="%.2f"/>\n', ...
                y,x,r,g,b,alpha) ;
            
-scale  = 3 ;
-width  = scale*(size(PROB.NICE,2)+ 17) ;
-height = scale*(size(PROB.NICE,1)+ 85) ;
+scale  = 500/max([size(PROB.NICE,1) size(PROB.NICE,2)]) ;
+% width  = scale*(size(PROB.NICE,2)+ 17) ;
+% height = scale*(size(PROB.NICE,1)+ 85) ;
+
+% scale = 1 ;
+% [width,height] = size(PROB.NICE) ;
+
+evidence = print_evidence( PROB.NICE ) ;
+% evidence = [sprintf('<g transform="scale(%f,%f)">',scale,scale) evidence '</g>'] ;
 
 fid = fopen('plot_cones_field_stub.svg') ;
-svg = sprintf(fread(fid,'*char'),width,height,5,20,width,height,scale,scale,evidence,svg) ;
+svg = sprintf(fread(fid,'*char'),scale,scale,evidence,svg) ;
 
 save_svg_plot(svg,'cones_field.svg')
 
