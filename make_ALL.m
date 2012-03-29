b83f0c057467081bd25935307575b56d60d59f8b
kolia <kolia.sadeghi@gmail.com>
2012-03-29 14:57:47 -0400

    routine changes

--- make_ALL.m
+++ make_ALL.m
@@ -18,26 +18,26 @@ make_plots( greed_peach_NROI2_ROI12_support3_SS4_5e05iters,...
             cast_peach_NROI2_ROI12_support3_SS4_5e05iters ) ; 
 % catch e, cd(this) ; end
 
-% try
-%     load('george/cast_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters')
-%     load('george/mcmc_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters')
-% catch
-%     addpath('../agricola')
-%     reap
-% 
-%     save('george/cast_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters',...
-%          'cast_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters')
-% 
-%     save('george/mcmc_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters',...
-%          'mcmc_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters')
-% end
-% % 
-% % clear *_peach_*
-% 
-% % [greed_george,cone_map_george,base_str_george] = main_CAST( 0 ) ;
-%  
-% this = pwd ;
-% make_plots( greed_george,...
-%             mcmc_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters,...
-%             cast_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters,...
-%             cone_map_george ) ;
+clear all
+
+try
+    load('george/cast_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters')
+    load('george/mcmc_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters')
+catch
+    addpath('../agricola')
+    reap
+
+    save('george/cast_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters',...
+         'cast_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters')
+
+    save('george/mcmc_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters',...
+         'mcmc_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters')
+end
+
+[greed_george,cone_map_george,base_str_george] = main_CAST( 0 ) ;
+ 
+this = pwd ;
+make_plots( greed_george,...
+            mcmc_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters,...
+            cast_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters,...
+            cone_map_george ) ;
