586c2903b6dd72309079f6374e1dceac51424685
kolia <kolia.sadeghi@gmail.com>
2012-03-29 14:18:37 -0400

    plot_LL_ncones excludes greedy from plot if isempty

--- plot_LL_ncones.m
+++ plot_LL_ncones.m
@@ -1,13 +1,17 @@
 function svg = plot_LL_ncones( greed , mcmc , cast , cone_map )
 
-[x,y] = get_best( [mcmc ; cast ; {greed}] ) ;
-id = [ones(numel(mcmc),1) ; 2*ones(numel(cast),1) ; 0] ;
+if ~isempty(greed)
+    [x,y] = get_best( [mcmc ; cast ; {greed}] ) ;
+    id = [ones(numel(mcmc),1) ; 2*ones(numel(cast),1) ; 0] ;
+else
+    [x,y] = get_best( [mcmc ; cast ] ) ;
+    id = [ones(numel(mcmc),1) ; 2*ones(numel(cast),1) ] ;
+end
 
 y = bits_per_spike( y, cone_map ) ;
 
-minx = min(x) ;% 99 ;
-maxx = max(x) ;%119 ;
-%inds = (x>minx) & (x<maxx) & (y>31000) ;
+minx = min(x) ;
+maxx = max(x) ;
 inds = (x>=minx) & (x<=maxx) ;
 
 id  = id(inds) ;
@@ -32,12 +36,13 @@ y  = y(inds) ;
 id = id(inds) ;
 
 M1 = max(y(id==1)) ;
-% M1i= y == M1 ;
 
 mxi = y == max(y) ;
 
 g_ncone = x(end) ;
 g_ll = y(end) ;
+
+mcmc_ncone = x(end-2) ;
 
 height = 220 ;
 
@@ -47,30 +52,38 @@ x = 200 * (x-minx)/(max(x)-minx) ;
 
 svg = sprints('<use xlink:href="#%d" transform="translate(%f %f)"/>\n',id,x,y) ;
 
-% svg = svg_xcoord( x(end) , y(end) , height ) ;
-% svg = svg_ycoord( x(end) , y(end) , height ) ;
-
-% svg = svg_ycoord( x(end) , y(end) , height ) ;
-
 ymax = y(find(x==max(x),1)) ;
 
-svg = [sprintf('<line x1="0" x2="%f" y1="%f" y2="%f" text-anchor="middle" stroke="black" opacity="0.4"/>\n',x(end),y(end),y(end)) ...
-       sprintf('<text x="-5" y="%d" font-size="12" text-anchor="end" baseline-shift="-45%%">%.4f</text>\n',y(end),g_ll) ...
-       sprintf('<line x1="%f" x2="%f" y1="%f" y2="%f" text-anchor="middle" stroke="black" opacity="0.4"/>\n',x(end),x(end),y(end),height) ...
-       sprintf('<text x="%f" y="%f" font-size="12" text-anchor="middle" baseline-shift="-110%%">%d</text>\n',x(end),height,g_ncone) ...
-       sprintf('<line x1="0" x2="%f" y1="0" y2="0" text-anchor="middle" stroke="black" opacity="0.4"/>\n',x(mxi)) ...
-       sprintf('<text x="-5" y="0" font-size="12" text-anchor="end" baseline-shift="-45%%">%.4f</text>\n',M) ...
+svg = [sprintf('<line x1="0" x2="%f" y1="0" y2="0" text-anchor="middle" stroke="black" opacity="0.4"/>\n',x(mxi)) ...
+       sprintf('<text x="-5" y="0" font-size="12" text-anchor="end" baseline-shift="-45%%">%.5f</text>\n',M) ...
        sprintf('<line x1="%f" x2="%f" y1="%f" y2="%f" text-anchor="middle" stroke="black" opacity="0.4"/>\n',200,200,ymax,height) ...
        sprintf('<text x="%f" y="%f" font-size="12" text-anchor="middle" baseline-shift="-110%%">%d</text>\n',200,height,ceil(Mx)) ...
        sprintf('<line x1="%f" x2="0" y1="%f" y2="%f" text-anchor="middle" stroke="black" opacity="0.4"/>\n',x(end-2),y(end-2),y(end-2)) ...
-       sprintf('<text x="-5" y="%f" font-size="12" text-anchor="middle" baseline-shift="-110%%">%.4f</text>\n',y(end-2),M1) ...
+       sprintf('<text x="-5" y="%f" font-size="12" text-anchor="middle" baseline-shift="-110%%">%.5f</text>\n',y(end-2),M1) ...
        sprintf('<text x="100" y="240" font-size="15" text-anchor="middle" baseline-shift="-100%%" font-weight="bold">     number of cones</text>\n') ...
        sprintf('<g transform="translate(-85 25)rotate(-90)"><text font-size="15" text-anchor="end" font-weight="bold">log posterior (bits/spike)</text></g>\n') ...
-       sprintf('<text x="75" y="-35" font-size="18" text-anchor="middle"><tspan fill="green">Greedy</tspan>, <tspan fill="blue">MCMC</tspan> and <tspan fill="red">CAST</tspan></text>\n') ...
        svg] ;
 
+if ~isempty(greed)
+    svg = [sprintf('<line x1="0" x2="%f" y1="%f" y2="%f" text-anchor="middle" stroke="black" opacity="0.4"/>\n',x(end),y(end),y(end)) ...
+           sprintf('<text x="-5" y="%d" font-size="12" text-anchor="end" baseline-shift="-45%%">%.4f</text>\n',y(end),g_ll) ...
+           sprintf('<line x1="%f" x2="%f" y1="%f" y2="%f" text-anchor="middle" stroke="black" opacity="0.4"/>\n',x(end),x(end),y(end),height) ...
+           sprintf('<text x="%f" y="%f" font-size="12" text-anchor="middle" baseline-shift="-110%%">%d</text>\n',x(end),height,g_ncone)...
+           sprintf('<text x="75" y="-35" font-size="18" text-anchor="middle"><tspan fill="green">Greedy</tspan>, <tspan fill="blue">MCMC</tspan> and <tspan fill="red">CAST</tspan></text>\n') ...
+           svg] ;
+else
+    svg = [sprintf('<line x1="%f" x2="%f" y1="%f" y2="%f" text-anchor="middle" stroke="black" opacity="0.4"/>\n',x(end-2),x(end-2),y(end-2),height) ...
+           sprintf('<text x="%f" y="%f" font-size="12" text-anchor="middle" baseline-shift="-110%%">%d</text>\n',x(end-2),height,mcmc_ncone)...
+sprintf('<text x="75" y="-35" font-size="18" text-anchor="middle"><tspan fill="blue">MCMC</tspan> and <tspan fill="red">CAST</tspan></text>\n') ...
+           svg] ;    
+end
    
 svg = insert_string(svg,'plot_LL_ncones_stub.svg',-40) ;
-save_svg_plot(svg,'LL_ncones.svg')
+
+if ~isempty(greed)
+    save_svg_plot(svg,'LL_ncones.svg')
+else
+    save_svg_plot(svg,'LL_ncones_nogreed.svg')
+end
 
 end
