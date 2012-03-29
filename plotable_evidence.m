c20453528aff7f3d8ecb6d4ce69d383c15531f85
kolia <kolia.sadeghi@gmail.com>
2012-03-29 09:27:32 -0400

    making dead zones grey

--- plotable_evidence.m
+++ plotable_evidence.m
@@ -13,7 +13,7 @@ evidence(inds3) = 0 ;
 evidence = evidence-min(evidence(:)) ;
 evidence = evidence / max(evidence(:)) ;
 
-evidence(inds3) = 1 ;
+evidence(inds3) = 0.8 ;
 evidence(infinite) = 0 ;
 
 end
