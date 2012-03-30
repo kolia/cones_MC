af0a3cd709d316a8344b16508d4bca7009971eb9
kolia <kolia.sadeghi@gmail.com>
2012-03-30 17:54:05 -0400

    routine changes

--- make_ALL.m
+++ make_ALL.m
@@ -1,24 +1,24 @@
-
-% save('peach/greed_peach_NROI2_ROI12_support3_SS4_5e05iters',...
-%      'greed_peach_NROI2_ROI12_support3_SS4_5e05iters')
-% save('peach/mcmc_peach_NROI2_ROI12_support3_SS4_5e05iters',...
-%      'mcmc_peach_NROI2_ROI12_support3_SS4_5e05iters')
-% save('peach/cast_peach_NROI2_ROI12_support3_SS4_5e05iters',...
-%      'cast_peach_NROI2_ROI12_support3_SS4_5e05iters')
-
-load('peach/greed_peach_NROI2_ROI12_support3_SS4_5e05iters')
-load('peach/mcmc_peach_NROI2_ROI12_support3_SS4_5e05iters')
-load('peach/cast_peach_NROI2_ROI12_support3_SS4_5e05iters')
-
- 
-this = pwd ; 
-% try
-make_plots( greed_peach_NROI2_ROI12_support3_SS4_5e05iters,...
-            mcmc_peach_NROI2_ROI12_support3_SS4_5e05iters,...
-            cast_peach_NROI2_ROI12_support3_SS4_5e05iters ) ; 
-% catch e, cd(this) ; end
-
-clear all
+% 
+% % save('peach/greed_peach_NROI2_ROI12_support3_SS4_5e05iters',...
+% %      'greed_peach_NROI2_ROI12_support3_SS4_5e05iters')
+% % save('peach/mcmc_peach_NROI2_ROI12_support3_SS4_5e05iters',...
+% %      'mcmc_peach_NROI2_ROI12_support3_SS4_5e05iters')
+% % save('peach/cast_peach_NROI2_ROI12_support3_SS4_5e05iters',...
+% %      'cast_peach_NROI2_ROI12_support3_SS4_5e05iters')
+% 
+% load('peach/greed_peach_NROI2_ROI12_support3_SS4_5e05iters')
+% load('peach/mcmc_peach_NROI2_ROI12_support3_SS4_5e05iters')
+% load('peach/cast_peach_NROI2_ROI12_support3_SS4_5e05iters')
+% 
+%  
+% this = pwd ; 
+% % try
+% make_plots( greed_peach_NROI2_ROI12_support3_SS4_5e05iters,...
+%             mcmc_peach_NROI2_ROI12_support3_SS4_5e05iters,...
+%             cast_peach_NROI2_ROI12_support3_SS4_5e05iters ) ; 
+% % catch e, cd(this) ; end
+% 
+% clear all
 
 try
     load('george/cast_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters')
@@ -35,7 +35,7 @@ catch
 end
 
 [greed_george,cone_map_george,base_str_george] = main_CAST( 0 ) ;
- 
+
 this = pwd ;
 make_plots( greed_george,...
             mcmc_george_NROI2_ROI12_support3_SS4_beta02_delta01_5e05iters,...
