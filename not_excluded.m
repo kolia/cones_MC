2ac9b1c7650aee07d621258cef8efbb3677521c9
kolia <kolia.sadeghi@gmail.com>
2012-04-05 12:56:54 -0400

    not_excluded returns relevant incides

--- not_excluded.m
+++ not_excluded.m
@@ -1,4 +1,4 @@
-function free = not_excluded(X,x,y)
+function [free,indices] = not_excluded(X,x,y)
 
 [~,indices] = place_mask( X.M0 , X.M1 , x , y , X.masks{1,1}.exclusion ) ;
 free    = isempty( find( X.state(indices)>0 , 1) ) ;
