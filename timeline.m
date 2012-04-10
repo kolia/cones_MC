8e9b7a75bbdf8afb04403c80e80d433406a83ab8
kolia <kolia.sadeghi@gmail.com>
2012-04-10 15:25:04 -0400

    simple timeline.m

--- timeline.m
+++ timeline.m
@@ -1,18 +1,29 @@
-function timeline()
+function timeline( greed , mcmc , cast, fast )
 
-% ( greed , mcmc , cast , cone_map )
+maxtime = max(greed.X.cputime) * 1.1 ;
 
-n = 1828 ;
-gr = [1:n ; cumsum(sort(0.6*rand(1,n),'descend')) ; zeros(1,n) ; zeros(1,n)] ;
+n_fast = fast.N_cones ;
 
-mc = cell(30,1) ;
-ca = cell(30,1) ;
+n  =  greed.X.iteration ;
+gr = [greed.X.cputime(1:n)' ; greed.X.LL_history(1:n)' ; zeros(1,n) ; zeros(1,n)] ;
 
-for i=1:30
-    n = ceil(1700+200*rand()) ;
-    mc{i} = [1:n ; cumsum(sort((0.9+0.2*rand())*rand(1,n),'descend')) ; zeros(1,n) ; zeros(1,n)] ;
-    n = ceil(1700+200*rand()) ;
-    ca{i} = [1:n ; cumsum(sort((1.2+0.2*rand())*rand(1,n),'descend')) ; zeros(1,n) ; zeros(1,n)] ;
+mc = cell(numel(mcmc),1) ;
+ca = cell(numel(cast),1) ;
+
+for i=1:numel(mcmc)
+    cputimes = reshift_cputime(mcmc{i}.X.cputime,fast) ;
+    n = mcmc{i}.X.iteration ;
+    cutoff = find(cputimes > maxtime,1) ;
+    if ~isempty(cutoff)
+        n = min( mcmc{i}.X.iteration, cutoff ) ;
+    end
+    mc{i} = [cputimes(1:n) ; fast.LL_history(1:n_fast)' mcmc{i}.X.LL_history(n_fast+(1:n-n_fast))' ; zeros(1,n) ; zeros(1,n)] ;
+end
+
+for i=1:numel(cast)
+    n = find(cast{i}.X.cputime > maxtime,1) ;
+    cputimes = reshift_cputime(cast{i}.X.cputime,fast) ;
+    ca{i} = [cputimes(1:n) ; fast.LL_history(1:n_fast)' cast{i}.X.LL_history(n_fast+(1:n-n_fast))' ; zeros(1,n) ; zeros(1,n)] ;
 end
 
 
@@ -36,18 +47,28 @@ for i=1:30
     ca{i}(3:4,:) = [width*ca{i}(1,:)/xmax ; height*ca{i}(2,:)/ymax] ;
 end
 
-svg = plot_line(gr(3,:),gr(4,:),'<line x1="%f" x2="%f" y1="%f" y2="%f" stroke="black"/>\n') ;
+svg = [] ;
 
-for i=1:30
-    svg = [svg plot_line(mc{i}(3,:),mc{i}(4,:),'<line x1="%f" x2="%f" y1="%f" y2="%f" stroke="blue"/>\n')] ;
-    svg = [svg plot_line(ca{i}(3,:),ca{i}(4,:),'<line x1="%f" x2="%f" y1="%f" y2="%f" stroke="red"/>\n')] ;
+for i=1:numel(mc)
+    svg = [svg plot_line(mc{i}(3,:),mc{i}(4,:),'<line x1="%f" x2="%f" y1="%f" y2="%f" stroke-width="0.1" stroke="blue"/>\n')] ;
 end
+
+for i=1:numel(ca)
+    svg = [svg plot_line(ca{i}(3,:),ca{i}(4,:),'<line x1="%f" x2="%f" y1="%f" y2="%f" stroke-width="0.1" stroke="red"/>\n')] ;
+end
+
+svg = [svg plot_line(gr(3,:),gr(4,:),'<line x1="%f" x2="%f" y1="%f" y2="%f" stroke-width="0.1" stroke="black"/>\n')] ;
 
 fid = fopen('timeline_stub.svg') ;
 svg = sprintf(fread(fid,'*char'),svg) ;
 
 save_svg_plot(svg,sprintf('timeline_%s.svg','test'))
 
+end
+
+function cputimes = reshift_cputime(cputimes,fast)
+m = fast.N_cones ;
+cputimes = [fast.cputime(1:m)' cputimes(m+1:end)'-cputimes(m+1)+fast.cputime(m)] - fast.cputime(1) ;
 end
 
 function svg = plot_line(x,y,pattern,points)
