53421f55403dec91e85bc517b1218fe18c77a209
kolia <kolia.sadeghi@gmail.com>
2012-04-18 15:05:54 -0400

    commented main for single-instance local runs

--- main.m
+++ main.m
@@ -1,49 +1,71 @@
+%% LOAD DATA
 
-% PREPARE cone_map
-
-load george/stas       
+% load george/stas       
+load peach/peach_data 
 % stas(i).spikes  : the list of spike times of cell i
 % stas(i).spatial : the spatial component of the STA of cell i
 
-load george/cone_params   
+% load george/cone_params   
+load peach/cone_params   
 % cone_params.stimulus_variance : variance of each stim pixel color chanel
-%                                 (default 1)
+%                                 (usually 1)
 %
 % cone_params.supersample       : the integer number of cone positions per
 %                                 pixel width/height
-%                                 (default 4)
+%                                 (usually 4)
 %
 % cone_params.colors            : 3x3 matrix of cone color sensitivities
 %
 % cone_params.support_radius    : radius of cone receptive field filter
-%                                 (default 3.0)
+%                                 (usually 3.0)
 % 
-% cone_params.repulsion_radii   : 
+% cone_params.repulsion_radii
 
-stas = restrict_ROI( stas, [1 160], [1 160] ) ;
 
-cone_params.support_radius = 3 ;
+% restrict the stimulus to a region of interest  (if memory is a problem)
+% stas = restrict_ROI( stas, [1 160], [1 160] ) ;
+
+
+
+%% CONE_MAP contains parameters and preprocessed data structures
+
+% calculate sparsity pattern (for MCMC) and initial LL map (for greedy)
 cone_map = exact_LL_setup(stas,cone_params) ;
 
-cone_map.N_iterations  = 1e6 ;
+% How many MCMC/CAST iterations?
+cone_map.N_iterations  = 1e4 ;
 cone_map.max_time      = 4e5 ;  % in seconds
+
+% Define the progression of inverse temperatures for CAST
 cone_map.min_delta = 0.1 ;
 cone_map.min_beta  = 0.2 ;
 cone_map.betas  = make_deltas( cone_map.min_beta, 1, 1, 20 ) ;
 cone_map.deltas = make_deltas( cone_map.min_delta, 1, 1, length(cone_map.betas) ) ;
 
-% cone_map.initX.supersample  = cone_params.supersample ;
-% cone_map.initX.support_radius = cone_params.support_radius ;
-% cone_map.initX.N_iterations   = cone_map.N_iterations ;
-% cone_map.initX.betas  = cone_map.betas  ;
-% cone_map.initX.deltas = cone_map.deltas ;
+% plot, display, and save every N iterations (0 = never)
+cone_map.plot_every    = 100 ;
+cone_map.display_every = 20  ;
+cone_map.save_every    = 0   ;
 
-cone_map.plot_every    =  0 ;   % zero means do not plot
-cone_map.display_every = 20 ;   % display info every 20 iterations
 
-greed_fast = greedy_cones(cone_map, 'fast') ;
-cone_map.initX = greed_fast.X ;
-
+%% GREEDY
 greed = greedy_cones(cone_map) ;  save('greed','greed') ;
+
+
+
+%% MCMC and CAST
+
+% track contacts during 'hot' greedy method
+cone_map.track_contacts = true ;
+
+% run 'hot' greedy method
+greed_hot = greedy_cones(cone_map, 'hot') ;
+
+% initialize MCMC and CAST with 'hot' greedy configuration
+cone_map.initX = greed_hot.X ;
+
+% run MCMC
 mcmc = MCMC(cone_map) ;           save('mcmc' ,'mcmc' ) ;
+
+% run CAST
 cast = CAST(cone_map) ;           save('cast' ,'cast' ) ;
