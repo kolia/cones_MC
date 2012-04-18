dd586de32a74ecc9fd34fa31e07e0ca1656361ba
kolia <kolia.sadeghi@gmail.com>
2012-04-18 14:59:52 -0400

    remake_X remakes X.excluded

--- remake_X.m
+++ remake_X.m
@@ -1,5 +1,7 @@
 function Y = remake_X( cone_map, X )
 
+Y = X ;
+
 if ~isfield(X,'WW') || ~isfield(X,'contact')
     Y = initialize_X( cone_map.M0, cone_map.M1, ...
                       cone_map.N_colors, cone_map.SS, ...
@@ -11,8 +13,15 @@ if ~isfield(X,'WW') || ~isfield(X,'contact')
         Y = flip_LL( Y , [x(i) y(i) c(i)] , cone_map , [1 1] ) ;
         Y = update_X({Y}) ;
     end
-else
-    Y = X ;
+end
+
+if ~isfield(X, 'excluded')
+    Y.excluded  = false(Y.M0,X.M1) ;
+    [x,y,c] = find(Y.state) ;
+    for i=1:numel(x)
+        [~,indices] = not_excluded( Y, x(i), y(i) ) ;
+        Y.excluded(indices) = true ;
+    end    
 end
 
 end
