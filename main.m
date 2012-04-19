274ea67f0e73904c37c6cc99eb67c6ed4b6fab75
kolia <kolia.sadeghi@gmail.com>
2012-04-19 14:03:10 -0400

    merged main and main_CAST

--- main.m
+++ main.m
@@ -1,33 +1,30 @@
+cone_map.datafolder = 'peach' ;
 addpath(genpath(pwd))
 
 %% LOAD DATA
 
-% load george/stas       
-load peach/peach_data 
-% stas(i).spikes  : the list of spike times of cell i
-% stas(i).spatial : the spatial component of the STA of cell i
+load(sprintf('%s/stas', cone_map.datafolder))  % stas struct contains fields:
+% stas(i).spikes    : the list of spike times of cell i
+% stas(i).spatial   : the spatial component of the STA of cell i
 
-% load george/cone_params   
-load peach/cone_params    % contains struct cone_params with fields:
+load(sprintf('%s/cone_params', cone_map.datafolder)) % cone_params struct fields:
 % stimulus_variance : variance of each stim pixel color chanel (usually 1)
 % supersample       : the integer number of cone positions per pixel
-%                     width/height  (usually 4) 
+%                     width/height  (usually 4)
 % colors            : 3x3 matrix of cone color sensitivities
 % support_radius    : radius of cone receptive field filter  (usually 3.0)
 % repulsion_radii
 
-
 % restrict the stimulus to a region of interest  (if memory is a problem)
 % stas = restrict_ROI( stas, [1 160], [1 160] ) ;
-
 
 %% CONE_MAP contains parameters and preprocessed data structures
 
 % calculate sparsity pattern (for MCMC) and initial LL map (for greedy)
-cone_map = exact_LL_setup(stas,cone_params) ;
+cone_map = exact_LL_setup(stas,cone_params,cone_map) ;
 
 % How many MCMC/CAST iterations?
-cone_map.N_iterations  = 1e4 ;
+cone_map.N_iterations  = 5e5 ;
 cone_map.max_time      = 4e5 ;  % in seconds
 
 % % override defaults: plot, display, and save every ? iterations (0 = never)
@@ -35,8 +32,23 @@ cone_map.max_time      = 4e5 ;  % in seconds
 % cone_map.display_every = 20 ;
 % cone_map.save_every    = 0  ;
 
+% string identifying this cone_map problem; useful for naming saved results
+cone_map.description = cone_map_string( cone_map ) ;
 
-%% run GREEDY, MCMC or CAST
-% greed = greedy_cones(cone_map) ;  save('greed','greed') ;
-% mcmc = MCMC(cone_map) ;           save('mcmc' ,'mcmc' ) ;
-cast = CAST(cone_map) ;           save('cast' ,'cast' ) ;
+%% run GREEDY, MCMC or CAST locally
+% greed = GREEDY(cone_map) ;  save(['greed_' cone_map.description],'greed') ;
+mcmc = MCMC(cone_map) ;           save(['mcmc_'  cone_map.description], 'mcmc') ;
+% cast = CAST(cone_map) ;           save(['cast_'  cone_map.description], 'cast') ;
+
+
+
+% %% or RUN 1 greedy instance, N MCMC instances and N CAST on the hpc cluster:
+% %  start by cloning AGRICOLA from https://github.com/kolia/agricola
+% addpath('../agricola')
+% N = 30 ;  ids = cell(1,N) ; for i=1:length(ids) , ids{i} = {i} ; end
+% 
+% PBS.l.mem = '1500mb' ;
+% PBS.l.walltime = '70:00:00' ;
+% sow(['cast_'  base_str],@(ID)CAST(cone_map,ID),ids,PBS) ;
+% sow(['mcmc_'  base_str],@(ID)MCMC(cone_map,ID),ids,PBS) ;
+% sow(['greed_' base_str],@(  )GREEDY(cone_map)) ;
