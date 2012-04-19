142a57a7c43a44488a22dca1a051acca0ee55a48
kolia <kolia.sadeghi@gmail.com>
2012-04-19 14:04:02 -0400

    renamed cone_map.type to cone_map.datafolder

--- cone_map_string.m
+++ cone_map_string.m
@@ -1,6 +1,6 @@
 function base_str = cone_map_string(cone_map)
 
-base_str  = sprintf('%s__%.0eiters', cone_map.type,cone_map.N_iterations) ;
+base_str  = sprintf('%s__%.0eiters', cone_map.datafolder,cone_map.N_iterations) ;
 
 base_str(base_str=='+') = '' ;
 base_str(base_str=='.') = '' ;
