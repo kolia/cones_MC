e02ed569b08d199a8dd899372bb23f4a25a362fa
kolia <kolia.sadeghi@gmail.com>
2012-04-23 22:10:29 -0400

    routine changes to main

--- main.m
+++ main.m
@@ -24,7 +24,7 @@ load(sprintf('%s/cone_params', cone_map.datafolder)) % cone_params struct fields
 cone_map = exact_LL_setup(stas,cone_params,cone_map) ;
 
 % How many MCMC/CAST iterations?
-cone_map.N_iterations  = 5e5 ;
+cone_map.N_iterations  = 1e4 ;
 cone_map.max_time      = 4e5 ;  % in seconds
 
 % % override defaults: plot, display, and save every ? iterations (0 = never)
@@ -36,16 +36,18 @@ cone_map.max_time      = 4e5 ;  % in seconds
 cone_map.description = cone_map_string( cone_map ) ;
 
 %% run GREEDY, MCMC or CAST locally
-% greed = GREEDY(cone_map) ;  save(['greed_' cone_map.description],'greed') ;
-mcmc = MCMC(cone_map) ;           save(['mcmc_'  cone_map.description], 'mcmc') ;
-% cast = CAST(cone_map) ;           save(['cast_'  cone_map.description], 'cast') ;
+% result = GREEDY(cone_map) ;  save(['greed_' cone_map.description],'result') ;
+result = MCMC(cone_map) ;           save(['mcmc_'  cone_map.description], 'result') ;
+% result = CAST(cone_map) ;           save(['cast_'  cone_map.description], 'result') ;
 
+%% some plots
+make_plots( result )
 
-
-% %% or RUN 1 greedy instance, N MCMC instances and N CAST on the hpc cluster:
+%% %% or RUN 1 greedy instance, N MCMC instances and N CAST on the hpc cluster:
 % %  start by cloning AGRICOLA from https://github.com/kolia/agricola
 % addpath('../agricola')
 % N = 30 ;  ids = cell(1,N) ; for i=1:length(ids) , ids{i} = {i} ; end
+% cone_map.save_disk_space = true ;  % strip results of large fields
 % 
 % PBS.l.mem = '1500mb' ;
 % PBS.l.walltime = '70:00:00' ;
