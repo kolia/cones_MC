5df548c05fdd56053744c420c78c75f9a3fa77d2
kolia <kolia.sadeghi@gmail.com>
2012-04-23 22:09:09 -0400

    svg_movie works with ROIs of varying sizes
    simplified layout; toggling works; starts with 'hot' greedy

--- dance.js
+++ dance.js
@@ -5,13 +5,14 @@ function startup(evt) {
   O=evt.target
   svgDocument=O.ownerDocument
   O.setAttribute("onmousedown","run=!run;dt()")
+  greedy_cones = svgDocument.getElementById("greedy_cones")
   cones = svgDocument.getElementById("cones")
-  itext = svgDocument.getElementById("itext")
+  itext    = svgDocument.getElementById("itext")
 }
 
-colors = ['red' , 'green' , '#66FF66'] ;
+colors = ['red' , 'green' , 'blue'] ;
 
-
+%s
 
 run    = true ;
 t      = 0 ;
@@ -23,9 +24,12 @@ function dt(){
   t = t+1 ;
   if (t>=data.length) {
     t = 0 ;
-    while(cones.hasChildNodes()) cones.removeChild(cones.firstChild);
+    for (i=0; i<4; i++){
+      these_cones = svgDocument.getElementById('cones'+i) ;
+      while(these_cones.hasChildNodes()) these_cones.removeChild(these_cones.firstChild);
+    }
   }
-  window.setTimeout("dt()",10)
+  window.setTimeout("dt()",2)
 }
 function iter(){
   for (i=0; i<data[t].length; i++){
@@ -39,15 +43,23 @@ function iter(){
 	cone = svgDocument.createElementNS(xmlns,'use') ;
 	cone.setAttributeNS(xlinkns,'xlink:href','#m'+dcone[3]) ;
 	cone.setAttributeNS(null,'transform','translate('+dcone[0]+' '+dcone[1]+')') ;
+	cone.setAttributeNS(null,'class','m'+dcone[3]) ;
 	cone.setAttributeNS(null,'id',dcone[0]+'_'+dcone[1]+'_'+dcone[3]) ;
       }
       cone.setAttribute('stroke',colors[dcone[2]-1]) ;
       cone.setAttribute('fill','none') ;
-      cones.appendChild(cone) ;
+      these_cones = svgDocument.getElementById('cones'+dcone[3]) ;
+      these_cones.appendChild(cone) ;
     }
   }
 }
 function toggle(id){
-  instance = svgDocument.getElementById(id) ;
-  instance.setAttributeNS(null,'visibility','hidden') ;
+  run = !run ;
+  instance = svgDocument.getElementById('cones'+id[1]) ;
+  if (instance.getAttribute("visibility")=="visible"){
+    instance.setAttributeNS(null,'visibility','hidden') ;
+  }
+  else {
+    instance.setAttributeNS(null,'visibility','visible') ;
+  }
 }
--- svg_movie.m
+++ svg_movie.m
@@ -1,39 +1,59 @@
-function svg = svg_movie( init , dX , greedy )
+function svg = svg_movie( init , dX , greedy, NICE )
 
 data = 'data = [' ;
-for i=1:1e10
-    idata = cell(numel(dX),4) ; run = 0 ;
+iterations = zeros(numel(dX),1) ;
+for j=1:numel(iterations)
+    iterations(j) = size(dX{j},1) ;
+end
+
+for i=[1 nnz(greedy):max(iterations)]
+    idata = cell(numel(dX),4) ;
     for j=1:numel(dX)
         if size(dX{j},1)>=i
-            if  i == 1 , idata(j,:) = state2xyc( init{j} , j ) ; end
             for jj=1:3 , idata{j,jj} = [idata{j,jj} dX{j}(i,jj:3:end)] ; end
             idata{j,4} = [idata{j,4} repmat(j,[1 numel(idata{j,1})])] ;
-            run = 1 ;
+            if  i == 1 , idata(j,:) = state2xyc( init{j} , j ) ; end
         end
     end
-    if ~run ,  break ; end
-    if i == 1 , idata = [idata ; state2xyc(greedy,0)] ; end
-    
-    ith = xycs2js( idata ) ;
-    if ~isempty( ith )
-        data = [data '[' ith sprintf('],\n')] ;
+    if i==max(iterations), break ; end
+    if i == 1
+        ss = state2xyc(greedy,0) ;
+        for k=1:4
+            idata{k} = [idata{k} ss{k}] ; 
+        end
     end
+    ith = xycs2js( idata ) ;
+%     if ~isempty( ith )
+        data = [data '[' ith sprintf('],\n')] ;
+%     end  
+%     if ~mod(i,100), fprintf('%d\n',i) ; end
 end
 data = [data(1:end-1) ']'] ;
 
-svg = insert_string( data , which('dance.js') , 356 ) ;
-svg = insert_string( svg  , which('dancing_cones_source.svg') , 172 ) ;
+scale  = 200/max([size(NICE,1) size(NICE,2)]) ;
+width  = min([200 200*size(NICE,2)/size(NICE,1)])+20 ;
+height = min([200 200*size(NICE,1)/size(NICE,2)])+20 ;
 
-try
-    lsres = ls('dancing_cones.svg') ;
-    user_input = input('\nWarning: rm old dancing_cones.svg? (y/n):','s') ;
-    switch user_input
-        case 'y'
-            delete('dancing_cones.svg')
-        otherwise
-            return
-    end
-end
+evidence = print_evidence( NICE ) ;
+
+fid = fopen('dance.js') ;
+js = sprintf(fread(fid,'*char'),data) ;
+fclose(fid) ;
+
+fid = fopen('dancing_cones_source.svg') ;
+svg = sprintf(fread(fid,'*char'),3*width,3*height,3*width,3*height,js,scale,scale,evidence) ;
+fclose(fid) ;
+
+% try
+%     lsres = ls('dancing_cones.svg') ;
+%     user_input = input('\nWarning: rm old dancing_cones.svg? (y/n): ','s') ;
+%     switch user_input
+%         case 'y'
+%             delete('dancing_cones.svg')
+%         otherwise
+%             return
+%     end
+% end
 fid = fopen('dancing_cones.svg','w') ;
 fwrite(fid,svg) ; fclose(fid) ;
 
