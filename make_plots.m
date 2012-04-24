f32eebaf3db77cbdf539dcd27ae19f0c5b99f2e0
kolia <kolia.sadeghi@gmail.com>
2012-04-23 22:07:40 -0400

    new make_plots plot whatever it can using results from CAST and MCMC

--- make_plots.m
+++ make_plots.m
@@ -1,42 +1,80 @@
-function make_plots(greed,mcmc,cast,cone_map)
+function make_plots( result, varargin )
+%    make_plots( X )
+% or make_plots( X, cone_map )
+% or make_plots( result )  
+%        where X = result.X and cone_map = result
+%        as returned by GREEDY, MCMC, and CAST
+%
+% makes whatever plots it can, saves them in folder
+% ['plots_for_' cone_map_string(result)]
 
-% if nargin<4
-%     cone_map = remake_cone_map(mcmc{1}) ;
-% end
+if iscell(result)
+    result = result{1} ;
+end
+
+bestX = [] ;
+if isfield(result,bestX)
+    bestX = result.bestX ;
+end
+
+if isfield(result,'state') % result is an X
+    X = result ;
+    if ~isempty(varargin)
+        cone_map = result ;        
+    else
+        cone_map = remake_cone_map( X ) ;
+    end
+else                       % result is a cone_map
+    X = result.X ;
+    if iscell(X), X = X{1} ; end
+    cone_map = rmfield(result,'X') ;
+end
+clear result
 
 folder_name = cone_map_string(cone_map);
 
+% place all plots in a folder
 filename = ['plots_for_' folder_name] ;
 mkdir( filename )
-
 here = pwd ;
-
 cd(filename)
 
-% [~,ll] = get_best( cast ) ;
-% 
-% try load(['../confident_' folder_name])
-% catch
-%     ['making ../confident_' folder_name]
-%     selector = @(n) (n>10000) && (mod(n,20) == 0) ;
-%     dX = cast{find(ll==max(ll),1)}.X.dX ;
-%     confident = confident_cones( cone_map.initX , dX , cone_map , selector ) ;
-% end
-% save(['../confident_' folder_name], 'confident') ;
-% plot_cone_field( confident , cone_map )
-% 
-% plot_LL_ncones( greed , mcmc , cast , cone_map )
-plot_LL_ncones( {}    , mcmc , cast , cone_map )
-% plot_Greedy_MCMC_CAST( greed , mcmc , cast , cone_map )
+try
+    % if possible, make cone field plot and movie; this requires dX
+    % CAST runs have a dX field that records all moves
+    if isfield( X, 'dX' )
+        try load('./confident')
+        catch
+            cone_map = remake_initX( cone_map, true ) ;
+            fprintf('making confident.mat\n')
+            % select which iterations to use; 10000 burnin, then every 20 iters
+            selector = @(n) (n>1000) && (mod(n,20) == 0) ;
+            confident = confident_cones( cone_map.initX , X.dX , cone_map , selector ) ;
+        end
+        save('confident','confident') ;
+            
+        svg_movie( {cone_map.initX.state} , {X.dX} , cone_map.initX.state, cone_map.NICE ) ;
+        
+        plot_cone_field( confident , cone_map )
+    end
 
-if strcmp(cone_map.type,'george')
-   timeline(greed,mcmc,cast,cone_map.initX, sum(cone_map.N_spikes)) 
+    if ~isempty(bestX)
+        for k=1:numel(bestX)
+            plot_cones( bestX{k}.state, cone_map ) ;
+            saveas(gcf,sprintf('best_cone_configuration_%d',k),'jpg')
+        end
+    else
+        plot_cones( X.state, cone_map ) ;
+        saveas(gcf,'final_cone_configuration','jpg')
+    end
+
+    % % [sta,invww] = denoised_sta( X.initX , X.dX , cone_map, selector ) ;
+    % % make_sta_plots( sta , invww, 'denoised' )
+
+    cd(here)
+catch err
+    cd(here)
+    rethrow(err);
 end
-
-
-% % [sta,invww] = denoised_sta( greed.initX , cast{1}.X.dX , cone_map, selector ) ;
-% % make_sta_plots( sta , invww, 'denoised' )
-
-cd(here)
 
 end
