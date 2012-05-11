be47b7a0a5f0e66e6d9dffa273e693d22aadcc9d
kolia <kolia.sadeghi@gmail.com>
2012-05-11 19:30:51 -0400

    move.m returns single proposal, calculated reverse proposal probe, for MH

--- move.m
+++ move.m
@@ -1,23 +1,20 @@
-function samples = move( X , PROB , T , n )
-% samples = move( X , PROB , n )
-% Draw n trial flips starting from state X.
+function sample = move( X , PROB , T )
+% sample = move( X , PROB )
+% Draw a trial flips starting from state X; calculated forward and backward
+% proposal probabilities.
 % Two types of move are produced: additions of cones, and moves or changes
 % in color of existing cones.
-% First, n spatial locations are drawn. They are drawn from existing cone
-% locations with probability PROB.q, and from the uniform distribution with
-% probability (1-PROB.q).
-% For each trial location i, if no cone of any color is at that position,
-% then the corresponding trial is a placement of a single randomly colored
-% cone at that location.
-% If a cone of any color is at location i, then the corresponding
+% First, a spatial location is drawn. It is drawn from existing cone
+% locations with probability PROB.q, and from the set of locations that are
+% available for adding a cone with probability (1-PROB.q).
+% For the trial location, if no cone of any color is at that position,
+% then the trial is a placement of a single randomly colored cone at that
+% location.
+% If a cone of any color is at the location, then the corresponding
 % trial is a change of color or deletion with probability 1/7, or a move of
 % the cone to a neighboring location, each with probability 1/7. If the
 % cone lies on the border of the domain, 7 is replaced by 3 + the number of
 % adjacent positions that are within the border.
-% For speed, backward probabilities are not calculated: this sampler should
-% only be used with the symmetric rule, not with metropolis-hastings.
-
-% check_X(X)
 
 if nargin<4 ,  n = 1 ; end
 
@@ -27,105 +24,109 @@ M1 = PROB.M1 * PROB.SS ;
 % current cones in X
 [cx,cy]     = find(X.state) ;
 
-% initialize samples
-samples  = cell(X.N_cones*7+n,1) ;
-ns       = 0 ;
+% probability of changing existing cone
+if X.N_cones >= X.maxcones  % not adding any more cones
+    q = 1 ;
+else
+    q = PROB.q ;
+end
+
 
 % propose moves of existing cones
-if X.N_cones > 0
-    % draw n_moved existing cones
-    n_moved     = binornd(n,PROB.q) ;
-    cones       = randi( X.N_cones , 1 , n_moved ) ;
+if X.N_cones>0  &&  rand()<q
     
-%     cones       = 1:X.n_cones ;
+    cone = randi(X.N_cones,1) ;
     
-    for s=1:n_moved
-%     for s=1:X.n_cones
-        i       = cx(cones(s)) ;
-        j       = cy(cones(s)) ;
-        color   = X.state(i,j) ;
-        
-        % probability of choosing this location
-        p = 1/X.N_cones * PROB.q ;
-        
-        % number of legal moves for this cone, being careful about borders
-        nforward    = 4 - PROB.outofbounds(i,j) + PROB.N_colors ;
+    i       = cx(cone) ;
+    j       = cy(cone) ;
+    color   = X.state(i,j) ;
 
-        % for each adjacent location, add trial move to that location
-        for d=1:4                 % move N , E , S , W
-            ni = i + X.masks{1,1}.shift{d}(1) ;
-            nj = j + X.masks{1,1}.shift{d}(2) ;
-            if ni > 0   &&  ni <= M0  &&  nj>0  &&  nj <= M1
-                ns = ns+1 ;
-%                 samples{ns} = move_cone( X , i , j , d , PROB , T ) ;                
-                samples{ns} = propagate_action(X,i+(j-1)*X.M0,d,PROB,T) ;
-                samples{ns}.forward_prob   = p/nforward ;
-            end
-        end
-        
-        % cone deletion
-        ns = ns+1 ;
-        samples{ns} = flip_LL( X , [i j 0] , PROB , T ) ;
-        samples{ns}.forward_prob    = p/nforward ;
-%         check_X(samples{ns})
+    % number of legal moves for this cone, being careful about borders
+    nforward = 4 - PROB.outofbounds(i,j) + PROB.N_colors ;
 
-        % change of color, without moving
-        deleted = ns ;
-        ne = not_excluded(X,i,j) ;
-        for cc=setdiff( 1:X.N_colors , color )
-            ns = ns+1 ;
-            if ne && ~isempty(PROB.sparse_struct{i,j,cc})
-                samples{ns} = flip_LL( samples{deleted} , [i j cc] , PROB , T ) ;
-%                 check_X(samples{ns})
+    % cone deletion
+    if rand()<1/nforward                    
+        sample = flip_LL( X , [i j 0] , PROB , T ) ;
+
+        % number of available new cone locations after removing cone
+        [~,indices] = not_excluded( X, i, j ) ;
+        excluded = X.excluded ;
+        excluded(indices) = false ;
+        N_not_excluded = M0*M1 - nnz(excluded) ;
+        
+        forward_probability = 1/X.N_cones * q / nforward ;
+
+        % probability of proposing reverse move: re-adding cone
+        reverse_probability = (1-PROB.q)/(N_not_excluded*PROB.N_colors) ;
+        
+        % proposal probability bias
+        sample.proposal_bias = forward_probability/reverse_probability ;
+
+    else
+        % change cone color
+        if rand()<(PROB.N_colors-1)/(nforward-1)
+            new_color = setdiff( 1:X.N_colors , color ) ;
+            new_color = new_color( randi(numel(new_color)) ) ;
+
+            ne = not_excluded(X,i,j) ;
+            if ne && ~isempty(PROB.sparse_struct{i,j,new_color})
+                sample = flip_LL( X , [i j new_color] , PROB , T ) ;
             else
-                samples{ns}    = samples{deleted} ;
-                samples{ns}.ll = -Inf ;
+                sample    = X ;
+                sample.ll = -Inf ;
             end
-            samples{ns}.forward_prob    = p/nforward ;
+            
+        % shift cone    
+        else
+            
+            % choose shift direction that is within bounds
+            while 1
+                d = randi(4,1) ;
+                ni = i + X.masks{1,1}.shift{d}(1) ;
+                nj = j + X.masks{1,1}.shift{d}(2) ;
+                if ni > 0   &&  ni <= M0  &&  nj>0  &&  nj <= M1
+                    break ;
+                end
+            end
+            sample = propagate_action(X,i+(j-1)*X.M0,d,PROB,T) ;
         end
         
+        % proposal probability bias
+        sample.proposal_bias = 1 ;
     end
+    
 else
-    n_moved = 0 ;
-end
+    
+    % sample unoccupied locations, propose cone additions
+    possible = find(~X.excluded & PROB.has_evidence) ;
+    [i,j] = ind2sub([M0 M1],possible(randi(numel(possible)))) ;
 
-% sample unoccupied locations, propose cone additions
-while ns <= n - n_moved
-    ex = find(~X.excluded & PROB.has_evidence) ;
-    [i,j] = ind2sub([M0 M1],ex(randi(numel(ex)))) ;
-%     i = ij(1) ;
-%     j = ij(2) ;
-%     i       = randi( M0 , 1 ) ;
-%     j       = randi( M1 , 1 ) ;
+    % forward probability of choosing this location & color
+    forward_probability = (1-q)/(numel(possible) * PROB.N_colors) ;
 
-    if ~X.state(i+M0*(j-1))
-        % probability of choosing this location & color
-        p = (1-PROB.q)/((M0*M1 - X.N_cones)*PROB.N_colors) ;
+    % choose new color
+    new_color = randi(PROB.N_colors) ;
 
-        if X.N_cones >= X.maxcones
-        % if maxcones has been reached, delete a random cone first
-            cone = randi( X.N_cones , 1 , 1 ) ;
-            X    = flip_LL( X, [cx(cone) cy(cone) 0], PROB, T ) ;
-            p    = p/X.N_cones ;
-        end
-
-        % propose addition of new cone of each color
-        ne = not_excluded(X,i,j) ;
-        for c=1:PROB.N_colors
-            ns = ns+1 ;
-            if ne && ~isempty(PROB.sparse_struct{i,j,c})
-                samples{ns} = flip_LL( X , [i j c] , PROB , T ) ;
-%                 check_X(samples{ns})
-            else
-                samples{ns}    = X ;
-                samples{ns}.ll = -Inf ;                
-            end
-            samples{ns}.forward_prob    = p ;
-        end
+    % propose addition of new cone of each color
+    ne = not_excluded(X,i,j) ;
+    if ne && ~isempty(PROB.sparse_struct{i,j,new_color})
+        sample = flip_LL( X , [i j new_color] , PROB , T ) ;
+    else
+        sample    = X ;
+        sample.ll = -Inf ;                
     end
+    
+    % probability of proposing reverse move: re-deleting cone
+    if X.N_cones >= X.maxcones  % not adding any more cones
+        q = 1 ;
+    else
+        q = PROB.q ;
+    end
+    nforward = 4 - PROB.outofbounds(i,j) + PROB.N_colors ;
+    reverse_probability = 1/X.N_cones * q / nforward ;
+    
+    % proposal probability bias
+    sample.proposal_bias = forward_probability/reverse_probability ;
 end
-
-samples = samples(1:ns) ;
-% fprintf('\nDONE sampling trial moves\n')
 
 end
