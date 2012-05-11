9c6e0fe370e95f75a6995b6e01f048d4d094da20
kolia <kolia.sadeghi@gmail.com>
2012-05-11 19:32:36 -0400

    slight change in LL, helps apparently

--- calculate_LL.m
+++ calculate_LL.m
@@ -2,7 +2,7 @@ function ll = calculate_LL( X , PROB , T )
 
 if X.N_cones>0
     ll  = 0.5 * T(1) * (sum( PROB.N_cones_terms .* sum(abs(X.sparse_STA_W_state)>0,2)) + ...
-            sum( PROB.quad_factors .* X.contributions.^T(2) ) ) ;
+            sum( (PROB.quad_factors .* X.contributions).^T(2) ) ) ;
 else
     ll = 0 ;
 end
